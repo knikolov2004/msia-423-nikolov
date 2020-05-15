b1e461047e5ce04416c1aa28521e6a0e07758202
Kristian Nikolov <knikolov2004@yahoo.com>
2020-05-15 15:45:28 +0300

    Adding script to create RDS database and fixing Dockerfile.

---
+++ src/create_db.py
@@ -0,0 +1,65 @@
+import logging.config
+
+logger = logging.getLogger(__name__)
+
+import sqlalchemy as sql
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Float, String, MetaData
+
+from config.config import ENGINE_STRING
+
+Base = declarative_base()
+
+
+class Features(Base):
+    """Create a data model for the database to be set up for adding songs and song features """
+    __tablename__ = "features"
+    danceability = Column(Float, unique=False, nullable=False)
+    energy = Column(Float, unique=False, nullable=False)
+    key = Column(Integer, unique=False, nullable=False)
+    loudness = Column(Float, unique=False, nullable=False)
+    mode = Column(Integer, unique=False, nullable=False)
+    speechiness = Column(Float, unique=False, nullable=False)
+    acousticness = Column(Float, unique=False, nullable=False)
+    instrumentalness = Column(Float, unique=False, nullable=False)
+    liveness = Column(Float, unique=False, nullable=False)
+    valence = Column(Float, unique=False, nullable=False)
+    tempo = Column(Float, unique=False, nullable=False)
+    type = Column(String(70), unique=False, nullable=False)
+    id = Column(String(70), primary_key=True)
+    uri = Column(String(70), unique=True, nullable=False)
+    track_href = Column(String(70), unique=True, nullable=False)
+    analysis_url = Column(String(70), unique=True, nullable=False)
+    duration_ms = Column(Integer, unique=False, nullable=False)
+    time_signature = Column(Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Features %r>' % self.id
+
+
+# set up mysql connection
+engine = sql.create_engine(ENGINE_STRING)
+
+# create the tracks table
+Base.metadata.create_all(engine)
+
+# UNCOMMENT THE CODE BELOW WHEN CODE THAT ADDS DATA IS INCORPORATED
+
+# # set up logging config
+# logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+# logger = logging.getLogger(__file__)
+#
+# # create a db session
+# Session = sessionmaker(bind=engine)
+# session = Session()
+#
+# # delete from table if not empty
+# try:
+#     session.execute('''DELETE FROM features''')
+# except:
+#     pass
+#
+# # ADD CODE FOR ADDING DATA FROM JSON TO DB HERE
+#
+# session.close()
--- src/upload_tracks.py
+++ src/upload_tracks.py
@@ -1,5 +1,6 @@
 import boto3
 import logging.config
+
 logger = logging.getLogger(__name__)
 # raw data names
 from config.config import WORKOUT_RAW_DATA, SLEEP_RAW_DATA, DINNER_RAW_DATA, PARTY_RAW_DATA
@@ -7,6 +8,7 @@ from config.config import WORKOUT_RAW_DATA, SLEEP_RAW_DATA, DINNER_RAW_DATA, PAR
 from config.config import WORKOUT_RAW_LOCATION, SLEEP_RAW_LOCATION, DINNER_RAW_LOCATION, PARTY_RAW_LOCATION
 # s3 bucket name
 from config.config import S3_BUCKET
+
 
 def upload_data_to_s3(bucket_name, local_file_path, s3_file_path):
     """ Upload data file to specified s3 bucket
@@ -16,7 +18,7 @@ def upload_data_to_s3(bucket_name, local_file_path, s3_file_path):
 
     Returns: Nothing
     """
-    logger.info("Uploading %s to S3:", s3_file_path)
+    logger.info("Uploading %s to S3 %s bucket as %s:", local_file_path, bucket_name, s3_file_path)
     s3 = boto3.resource("s3")
     bucket = s3.Bucket(bucket_name)
     try:
@@ -24,6 +26,7 @@ def upload_data_to_s3(bucket_name, local_file_path, s3_file_path):
         logger.info("File successfully uploaded.")
     except:
         logger.error("Could not upload file. Please check if the bucket name and local file path provided are valid.")
+
 
 if __name__ == "__main__":
     """
